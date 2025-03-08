f875e9312008d63dd21603b3676b2dc100dbedbc
amongus-pvp <130636031+amongus-pvp@users.noreply.github.com>
2025-03-08 14:11:03 +1000

    Update SittingOrStanding.py

--- Starter Problems/SittingOrStanding.py
+++ Starter Problems/SittingOrStanding.py
@@ -1,5 +1,5 @@
-r, s = map(int, input_line.split())
-num_tickets = int(input_file.readline().strip())
+r, s = map(int, input().split())
+num_tickets = int(input().strip())
 
 num_sitting = min(r*s, num_tickets)
 if r*s >= num_tickets:
