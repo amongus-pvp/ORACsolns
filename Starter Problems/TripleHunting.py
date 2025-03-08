6bec947b8ac33ef8b1b3fc6a309091d591238524
amongus-pvp <130636031+amongus-pvp@users.noreply.github.com>
2025-03-08 14:23:43 +1000

    Create TripleHunting.py

---
+++ Starter Problems/TripleHunting.py
@@ -0,0 +1,19 @@
+arr = []
+n = int(input())
+for i in range(n):
+    arr.append(int(input()))
+ 
+count = 0
+positions = []
+ 
+for i in range(len(arr)):
+    if arr[i]%3==0:
+        count += 1
+        positions.append(i+1)
+ 
+if count != 0:
+    print(str(count))
+    for i in range(len(positions)):
+        print(str(positions[i]))
+else:
+    print("Nothing here!")
