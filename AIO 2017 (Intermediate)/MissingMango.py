69e05066a4d2abdb5e5939d800017e642e3a1ae7
amongus-pvp <130636031+amongus-pvp@users.noreply.github.com>
2025-03-11 13:43:55 +1000

    Create MissingMango.py

---
+++ AIO 2017 (Intermediate)/MissingMango.py
@@ -0,0 +1,10 @@
+# https://orac2.info/problem/204/
+I, C, ID, CD = map(int, input().split())
+ 
+s1, s2 = I + ID, I - ID
+s3, s4 = C + CD, C - CD
+ 
+if s1 == s3 or s1 == s4:
+    print(s1)
+else:
+    print(s2)
