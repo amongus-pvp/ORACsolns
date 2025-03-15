74e77ec47dcfc692cb39b99c72b7f67d6fb5b645
amongus-pvp <130636031+amongus-pvp@users.noreply.github.com>
2025-03-15 15:43:56 +1000

    Create NomNomNom.py

---
+++ AIO 2009 (Intermediate)/NomNomNom.py
@@ -0,0 +1,20 @@
+# https://orac2.info/problem/39/
+N = int(input())
+arr = []
+for i in range(N):
+    arr.append(int(input()))
+count = 1
+num = arr[0]
+arr.pop(0)
+l = 0
+r = 0
+while l<N-1 and r<N-1:
+    s = sum(arr[l:r+1])
+    if s < num:
+        r += 1
+    elif s >= num:
+        count += 1
+        l = r+1
+        r += 1
+        num = s
+print(count)
