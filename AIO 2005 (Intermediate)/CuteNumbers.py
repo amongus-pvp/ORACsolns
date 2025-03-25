ab9a96f8c8ae2f6ea33d56b63bb0d9567b196b91
amongus-pvp <130636031+amongus-pvp@users.noreply.github.com>
2025-03-26 01:57:20 +1000

    Create CuteNumbers.py

---
+++ AIO 2005 (Intermediate)/CuteNumbers.py
@@ -0,0 +1,15 @@
+# https://orac2.info/problem/175/
+d = int(input())
+arr = []
+for i in range(d):
+    x = int(input())
+    arr.append(x)
+ 
+length = len(arr)
+cuteness = 0
+for i in range(length):
+    if arr[length-i-1] == 0:
+        cuteness += 1
+    else:
+        break
+print(cuteness)
