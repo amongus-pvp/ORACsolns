87695e717acb72e7520294f12f91af03834a5238
amongus-pvp <130636031+amongus-pvp@users.noreply.github.com>
2025-03-26 21:42:57 +1000

    Create CastleCavalry.py

---
+++ AIO 2018 (Intermediate)/CastleCavalry.py
@@ -0,0 +1,19 @@
+# https://orac2.info/problem/321/
+N = int(input())
+g = {}
+for i in range(N):
+    c = int(input())
+    if c in g:
+        g[c] += 1
+    else:
+        g[c] = 1
+ 
+p = True
+for i in g:
+    if g[i]%i != 0:
+        p = False
+
+if p:
+    print("YES")
+else:
+    print("NO")
