2af5e69441f0227ec752880cfa146170d51e126c
amongus-pvp <130636031+amongus-pvp@users.noreply.github.com>
2025-03-26 21:39:15 +1000

    Create ChimeraII.py

---
+++ AIO 2017 (Senior)/ChimeraII.py
@@ -0,0 +1,47 @@
+# https://orac2.info/problem/308/
+N = int(input())
+top = list(input())
+bottom = list(input())
+target = list(input())
+ 
+switches = 0
+status = True
+# js put the fries in the bag lil bro
+# translating ts from C++ was a pain in the ass :pray:
+state = 3
+ 
+for i in range(N):
+    if state == 1:
+        if top[i] == target[i]:
+            state = 1
+        elif bottom[i] == target[i]:
+            switches += 1
+            state = 2
+        else:
+            status = False
+            break
+    elif state == 2:
+        if bottom[i] == target[i]:
+            state = 2
+        elif top[i] == target[i]:
+            switches += 1
+            state = 1
+        else:
+            status = False
+            break
+    elif state == 3:
+        if top[i] == target[i] and bottom[i] == target[i]:
+            state = 3
+        elif bottom[i] == target[i]:
+            state = 2
+        elif top[i] == target[i]:
+            state = 1
+        else:
+            status = False
+            break
+ 
+if status == True:
+    print('SUCCESS')
+    print(switches)
+else:
+    print('PLAN FOILED')
